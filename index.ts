6df8b61c22c920ac3bc0fb2ca5120046a3f04a05
Alen Toma <alen.toma@gmail.com>
2021-09-10 20:44:31 +0200

    add a waiting list

--- index.ts
+++ index.ts
@@ -8,26 +8,35 @@ const wait = (time: number) => {
   });
 }
 
-const set = async (value: any, keyValue: any, key: string, setValue: Function, parent: any) => {
-  while (parent.wait)
-    await wait(200);
-  try {
-    parent.wait = true;
+const addToWait = async <T>(value: any, keyValue: any, key: string, setValue: Function, parent: UseState<T>) => {
+  parent.watingList.push(async () => await set(value, keyValue, key, setValue, parent));
+  if (parent.isWorking)
+    return;
 
-    if (value != keyValue) await setValue(value);
-    parent.wait = false;
+  parent.isWorking = true;
+  while (parent.watingList.length > 0) {
+    await parent.watingList[0]();
+  }
+
+  parent.isWorking = false;
+}
+
+const set = async <T>(value: any, keyValue: any, key: string, setValue: Function, parent: UseState<T>) => {
+  try {
+    await setValue(value);
+    parent.watingList.shift();
   } catch (error) {
     console.log(error);
-    parent.wait = false;
+    parent.watingList.shift();
     throw error;
   }
 }
 
-const assign = (item: any, assignTo: any) => {
+const assign = <T>(item: any, assignTo: UseState<T>) => {
   var tItem = item as any;
   Object.keys(item).forEach((key) => {
-    var thisAny = assignTo as any;
-    if (
+    const thisAny = assignTo as any;
+    if (assignTo.hierarkiUseState &&
       tItem[key] &&
       typeof tItem[key] === 'object' &&
       tItem[key] !== null &&
@@ -42,15 +51,22 @@ const assign = (item: any, assignTo: any) => {
           return keyValue;
         },
         set(value) {
-          set(value, keyValue, key, setValue, assignTo);
+          return addToWait<T>(value, keyValue, key, setValue, assignTo);
         },
       });
     }
   });
 };
 
+
 class UseState<T> {
-  constructor(item: T) {
+  watingList: any[]
+  isWorking: boolean;
+  hierarkiUseState: boolean;
+  constructor(item: T, hierarkiUseState?: boolean) {
+    this.watingList = [];
+    this.isWorking = false;
+    this.hierarkiUseState = hierarkiUseState ?? true;
     assign(item, this);
   }
 
@@ -58,14 +74,14 @@ class UseState<T> {
     const thisAny = this as any;
     Object.keys(item).forEach((x) => {
       var v = (item as any)[x];
-      if (v && typeof v === 'object' && v !== null && !Array.isArray(v))
+      if (this.hierarkiUseState && v && typeof v === 'object' && v !== null && !Array.isArray(v))
         thisAny[x].setValue(v);
       else thisAny[x] = v;
     });
   };
 }
 
-export default <T>(item: T) => {
-  const state = new UseState<T>(item) as T | UseState<T>;
+export default <T>(item: T, hierarkiUseState?: boolean) => {
+  const state = new UseState<T>(item, hierarkiUseState) as T | UseState<T>;
   return state as T;
 };
